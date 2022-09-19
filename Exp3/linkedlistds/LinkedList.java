89f25f0b1a275b259dd843eab2f2504d0b6a8350
Hatim Sawai <hatimsawai76@gmail.com>
2022-09-20 00:27:37 +0530

    Singly Linked List v1
    Signed-off-by: Hatim Sawai <hatimsawai76@gmail.com>

--- Exp3/linkedlistds/LinkedList.java
+++ Exp3/linkedlistds/LinkedList.java
@@ -2,24 +2,19 @@ package linkedlistds;
 class ListNode {
     private int data;
     private ListNode next;
-
     public ListNode(int data) {
         this.data = data;
         next = null;
     }
-
     public int getData() {
         return data;
     }
-
     public void setData(int data) {
         this.data = data;
     }
-
     public ListNode getNext() {
         return next;
     }
-
     public void setNext(ListNode next) {
         this.next = next;
     }
