845377d6449b4e9880e24330dbafc608edb077f5
Hitstar53 <84141920+Hitstar53@users.noreply.github.com>
2022-09-22 15:39:21 +0530

    Update ListCheck.java

--- Exp3/ListCheck.java
+++ Exp3/ListCheck.java
@@ -1,68 +1,109 @@
-import java.util.Scanner;
-import linkedlistds.LinkedList;
-public class ListCheck {
-    public static void main(String[] args) {
+import java.util.*;
+public class polyAdd {
+    Node head;
+    class Node{
+        int cof;
+        int exp;
+        Node next;
+        Node(int cof, int exp){
+            this.cof=cof;
+            this.exp=exp;
+            next=null;
+        }
+    }
+
+    void addLast(int cof, int exp){
+        Node newNode = new Node(cof, exp);
+        if(head==null){
+            head=newNode;
+            return;
+        }
+
+        Node currNode=head;
+        while(currNode.next!=null){
+            currNode=currNode.next;
+        }
+        currNode.next=newNode;
+    }
+
+    void print(){
+        Node currNode=head;
+        while(currNode!=null){
+            System.out.print(currNode.cof+"X^"+currNode.exp+" + ");
+            currNode=currNode.next;
+        }
+        System.out.print("NULL");
+    }
+
+    
+
+    public static void main(String [] args){
+        polyAdd p1 = new polyAdd();
+        polyAdd p2 = new polyAdd();
+
+        polyAdd p3 = new polyAdd();
         Scanner sc = new Scanner(System.in);
-        LinkedList list = new LinkedList();
-        int choice,flag,pos;
-        while(true) {
-            System.out.println(
-                    "Select an option:\n1.Insert at front\t2.Insert at pos\n3.Insert at end\n4.Delete at front\t5.Delete at pos\n6.Delete at end");
-            choice = sc.nextInt();
-            switch(choice) {
-                case 1:
-                    System.out.println("Enter the element to be inserted:");
-                    list.insertAtFront(sc.nextInt());
-                    System.out.println("The list is: " + list.printList());
-                    break;
-                case 2:
-                    System.out.println("Enter the element to be inserted:");
-                    int data = sc.nextInt();
-                    System.out.println("Enter the Element after which to insert:");
-                    pos = list.getPosition(sc.nextInt());
-                    if (pos == -1) {
-                        System.out.println("Element not found!");
-                    } else {
-                        pos = pos + 1;
-                        list.insertAtPosition(data, pos);
-                        System.out.println("The list is: " + list.printList());
-                    }
-                    break;
-                case 3:
-                    System.out.println("Enter the element to be inserted:");
-                    list.insertAtEnd(sc.nextInt());
-                    System.out.println("The list is: " + list.printList());
-                    break;
-                case 4:
-                    System.out.println("Deleted element: "+list.deleteAtFront());
-                    System.out.println("The list is: " + list.printList());
-                    break;
-                case 5:
-                    System.out.println("Enter the Element to be deleted:");
-                    pos = list.getPosition(sc.nextInt());
-                    if(pos == -1) {
-                        System.out.println("Element not found!");
-                    } else {
-                        System.out.println("Deleted element: "+list.deleteAtPosition(pos+1));
-                        System.out.println("The list is: " + list.printList());
-                    }
-                    break;
-                case 6:
-                    System.out.println("Deleted element: "+list.deleteAtEnd());
-                    System.out.println("The list is: " + list.printList());
-                    break;
-                case 7:
-                    System.out.println("The list is: "+list.printList());
-                    break;
-                default:
-                    System.out.println("Invalid choice");
+
+        System.out.println("Enter no. of terms your 1st polynomial has:");
+        int len1=sc.nextInt();
+        for(int i=0; i<len1; i++){
+            System.out.println("Term"+(i+1));
+            System.out.println("Enter coefficent");
+            int cof = sc.nextInt();
+            System.out.println("Enter exponent");
+            int exp = sc.nextInt();
+            p1.addLast(cof,exp);
+        }
+
+        System.out.println("Enter no. of terms your 2nd polynomial has:");
+        int len2=sc.nextInt();
+        for(int i=0; i<len2; i++){
+            System.out.println("Term"+(i+1));
+            System.out.println("Enter coefficent");
+            int cof = sc.nextInt();
+            System.out.println("Enter exponent");
+            int exp = sc.nextInt();
+            p2.addLast(cof,exp);
+        }
+    
+        
+
+        Node p = p1.head;
+        Node q = p2.head;
+        while(p!=null&& q!=null){
+            if(p.exp==q.exp){
+                p3.addLast(p.cof+q.cof, p.exp);
+                p=p.next;
+                q=q.next;
             }
-            System.out.println("Do you want to continue?\n1. Yes\t2. No");
-            flag = sc.nextInt();
-            if (flag == 2) {
-                break;
+            else{
+                if(p.exp>q.exp){
+                    p3.addLast(p.cof, p.exp);
+                    p=p.next;
+                }else{
+                    p3.addLast(q.cof, q.exp);
+                    q=q.next;
+                }
             }
         }
-        sc.close();
+
+        while(p!=null){
+            p3.addLast(p.cof, p.exp);
+            p=p.next;
+        }
+        while(q!=null){
+            p3.addLast(q.cof, q.exp);
+            q=q.next;
+        }
+        
+        System.out.println("Polynomial 1");
+        p1.print();
+        System.out.println();
+        System.out.println("Polynomial 2");
+        p2.print();
+        System.out.println();
+        System.out.println("Final Answer:");
+        p3.print();
+
     }
 }
