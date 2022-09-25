d9baa62d7cc3b2582e6ed4d1e59f46e1772625dc
Hatim Sawai <hatimsawai76@gmail.com>
2022-09-25 17:15:02 +0530

    doubly linked list
    Signed-off-by: Hatim Sawai <hatimsawai76@gmail.com>

--- Exp4/DLLCheck.java
+++ Exp4/DLLCheck.java
@@ -1,7 +1,67 @@
 import java.util.Scanner;
+import dlinkedlistds.DLL;
 public class DLLCheck {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        DLL d = new DLL();
+        int choice,flag,pos;
+        while(true) {
+            System.out.println(
+                    "Select an option:\n1.Insert at front\t2.Insert at pos\n3.Insert at end\n4.Delete at front\t5.Delete at pos\n6.Delete at end");
+            choice = sc.nextInt();
+            switch(choice) {
+                case 1:
+                    System.out.println("Enter the element to be inserted:");
+                    d.insertAtFront(sc.nextInt());
+                    System.out.println("The list is: " + d.printList());
+                    break;
+                case 2:
+                    System.out.println("Enter the element to be inserted:");
+                    int data = sc.nextInt();
+                    System.out.println("Enter the Element after which to insert:");
+                    pos = d.getPos(sc.nextInt());
+                    if (pos == -1) {
+                        System.out.println("Element not found!");
+                    } else {
+                        d.insertAtPos(data, pos+1);
+                        System.out.println("The list is: " + d.printList());
+                    }
+                    break;
+                case 3:
+                    System.out.println("Enter the element to be inserted:");
+                    d.insertAtEnd(sc.nextInt());
+                    System.out.println("The list is: " + d.printList());
+                    break;
+                case 4:
+                    System.out.println("Deleted element: "+d.deleteAtFront());
+                    System.out.println("The list is: " + d.printList());
+                    break;
+                case 5:
+                    System.out.println("Enter the Element to be deleted:");
+                    pos = d.getPos(sc.nextInt());
+                    if(pos == -1) {
+                        System.out.println("Element not found!");
+                    } else {
+                        System.out.println("Deleted element: "+d.deleteAtPos(pos));
+                        System.out.println("The list is: " + d.printList());
+                    }
+                    break;
+                case 6:
+                    System.out.println("Deleted element: "+d.deleteAtEnd());
+                    System.out.println("The list is: " + d.printList());
+                    break;
+                case 7:
+                    System.out.println("The list is: "+d.printList());
+                    break;
+                default:
+                    System.out.println("Invalid choice!");
+            }
+            System.out.println("Do you want to continue?\n1. Yes\t2. No");
+            flag = sc.nextInt();
+            if (flag == 2) {
+                break;
+            }
+        }
         sc.close();
     }
 }
--- Exp4/dlinkedlistds/DLL.java
+++ Exp4/dlinkedlistds/DLL.java
@@ -1,4 +1,131 @@
 package dlinkedlistds;
 public class DLL {
-    
+    class Node {
+        int data;
+        Node next,prev;
+        public Node(int data) {
+            this.data = data;
+            next = null;
+            prev = null;
+        }
+    }
+    Node head=null;
+    Node tail=null;
+    int len;
+    public void insertAtFront(int data) {
+        Node newNode = new Node(data);
+        if(head==null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            head.prev = newNode;
+            newNode.next = head;
+            head = newNode;
+        }
+        len++;
+    }
+    public void insertAtEnd(int data) {
+        Node newNode = new Node(data);
+        if(head==null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        len++;
+    }
+    public void insertAtPos(int data,int pos) {
+        if(pos<1||pos>len+1) {
+            System.out.println("Invalid position");
+        } else if(pos==1) {
+            insertAtFront(data);
+        } else if(pos==len+1) {
+            insertAtEnd(data);
+        } else {
+            Node newNode = new Node(data);
+            Node temp = head;
+            for(int i=1;i<pos-1;i++) {
+                temp = temp.next;
+            }
+            newNode.next = temp.next;
+            temp.next.prev = newNode;
+            temp.next = newNode;
+            newNode.prev = temp;
+            len++;
+        }
+    }
+    public int deleteAtFront() {
+        if(head==null) {
+            System.out.println("List is empty");
+            return -1;
+        } else {
+            int data = head.data;
+            head = head.next;
+            head.prev = null;
+            len--;
+            return data;
+        }
+    }
+    public int deleteAtEnd() {
+        if(head==null) {
+            System.out.println("List is empty");
+            return -1;
+        } else if(len==1) {
+            int data = head.data;
+            head = null;
+            tail = null;
+            len--;
+            return data;
+        } else {
+            int data = tail.data;
+            tail = tail.prev;
+            tail.next = null;
+            len--;
+            return data;
+        }
+    }
+    public int deleteAtPos(int pos) {
+        if(pos<1||pos>len) {
+            System.out.println("Invalid position");
+            return -1;
+        } else if(pos==1) {
+            return deleteAtFront();
+        } else if(pos==len) {
+            return deleteAtEnd();
+        } else {
+            Node temp = head;
+            for(int i=1;i<pos-1;i++) {
+                temp = temp.next;
+            }
+            int data = temp.next.data;
+            temp.next = temp.next.next;
+            temp.next.prev = temp;
+            len--;
+            return data;
+        }
+    }
+    public String printList() {
+        Node temp = head;
+        String s = "head->";
+        while(temp!=null) {
+            s += temp.data+(temp.next!=null?"->":"");
+            temp = temp.next;
+        }
+        s += "<-tail";
+        return s;
+    }
+    public int getPos(int data) {
+        Node temp = head;
+        int pos = 1;
+        while(temp!=null) {
+            if(temp.data==data) {
+                return pos;
+            }
+            temp = temp.next;
+            pos++;
+        }
+        return -1;
+    }
 }
