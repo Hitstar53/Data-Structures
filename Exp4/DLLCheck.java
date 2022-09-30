27e31eeb0b6b3c13c0f693d516ddf472d7b6a8b7
Hatim Sawai <hatimsawai76@gmail.com>
2022-09-30 21:14:53 +0530

    DLL final program
    Signed-off-by: Hatim Sawai <hatimsawai76@gmail.com>

--- Exp4/DLLCheck.java
+++ Exp4/DLLCheck.java
@@ -4,22 +4,22 @@ public class DLLCheck {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         DLL d = new DLL();
-        int choice,flag,pos;
+        int choice,flag,pos,data;
         while(true) {
             System.out.println(
-                    "Select an option:\n1.Insert at front\t2.Insert at pos\n3.Insert at end\n4.Delete at front\t5.Delete at pos\n6.Delete at end");
+                    "Select an option:\n1.Insert at front\n2.Insert at Right\n3.Insert at left\n4.Insert at end\n5.Delete at front\n6.Delete at right\n7.Delete at left\n8.Delete at end");
             choice = sc.nextInt();
             switch(choice) {
                 case 1:
-                    System.out.println("Enter the element to be inserted:");
+                    System.out.print("Enter the element to be inserted: ");
                     d.insertAtFront(sc.nextInt());
                     System.out.println("The list is: " + d.printList());
                     break;
                 case 2:
-                    System.out.println("Enter the element to be inserted:");
-                    int data = sc.nextInt();
-                    System.out.println("Enter the Element after which to insert:");
-                    pos = d.getPos(sc.nextInt());
+                    System.out.print("Enter the element to be inserted: ");
+                    data = sc.nextInt();
+                    System.out.print("Enter the pos after which to insert: ");
+                    pos = sc.nextInt();
                     if (pos == -1) {
                         System.out.println("Element not found!");
                     } else {
@@ -28,29 +28,51 @@ public class DLLCheck {
                     }
                     break;
                 case 3:
-                    System.out.println("Enter the element to be inserted:");
-                    d.insertAtEnd(sc.nextInt());
-                    System.out.println("The list is: " + d.printList());
-                    break;
-                case 4:
-                    System.out.println("Deleted element: "+d.deleteAtFront());
-                    System.out.println("The list is: " + d.printList());
-                    break;
-                case 5:
-                    System.out.println("Enter the Element to be deleted:");
-                    pos = d.getPos(sc.nextInt());
-                    if(pos == -1) {
+                    System.out.print("Enter the element to be inserted: ");
+                    data = sc.nextInt();
+                    System.out.print("Enter the pos before which to insert: ");
+                    pos = sc.nextInt();
+                    if (pos == -1) {
                         System.out.println("Element not found!");
                     } else {
-                        System.out.println("Deleted element: "+d.deleteAtPos(pos));
+                        d.insertAtPos(data,pos);
                         System.out.println("The list is: " + d.printList());
                     }
                     break;
+                case 4:
+                    System.out.print("Enter the element to be inserted: ");
+                    d.insertAtEnd(sc.nextInt());
+                    System.out.println("The list is: " + d.printList());
+                    break;
+                case 5:
+                    System.out.println("Deleted element: "+d.deleteAtFront());
+                    System.out.println("The list is: " + d.printList());
+                    break;
                 case 6:
+                    System.out.print("Enter pos after which to delete: ");
+                    pos = sc.nextInt();
+                    if(pos == -1) {
+                        System.out.println("Element not found!");
+                    } else {
+                        System.out.println("Deleted element: "+d.deleteAtPos(pos+1));
+                        System.out.println("The list is: " + d.printList());
+                    }
+                    break;
+                case 7:
+                    System.out.print("Enter pos before which to delete: ");
+                    pos = sc.nextInt();
+                    if (pos == -1) {
+                        System.out.println("Element not found!");
+                    } else {
+                        System.out.println("Deleted element: " + d.deleteAtPos(pos-1));
+                        System.out.println("The list is: " + d.printList());
+                    }
+                    break;
+                case 8:
                     System.out.println("Deleted element: "+d.deleteAtEnd());
                     System.out.println("The list is: " + d.printList());
                     break;
-                case 7:
+                case 9:
                     System.out.println("The list is: "+d.printList());
                     break;
                 default:
