4ea4a73b20297c72ad6cc9d582009be777c60819
Hatim Sawai <hatimsawai76@gmail.com>
2022-11-03 08:13:15 +0530

    graph exp initial
    Signed-off-by: Hatim Sawai <hatimsawai76@gmail.com>

--- Exp8/GraphCheck.java
+++ Exp8/GraphCheck.java
@@ -1,21 +1,23 @@
 import java.util.Scanner;
-import graphds.MatrixGraph;
+import graphds.LLGraph;
 public class GraphCheck {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of vertices: ");
-        int n = sc.nextInt();
-        MatrixGraph g = new MatrixGraph(n);
-        System.out.println("Enter the edges: ");
-        while (true) {
-            int u = sc.nextInt();
-            int v = sc.nextInt();
-            if (u == -1 && v == -1) {
-                break;
-            }
-            g.addEdge(u, v);
+        int v = sc.nextInt();
+        LLGraph g = new LLGraph(v);
+        System.out.print("Enter the number of edges: ");
+        int e = sc.nextInt();
+        for(int i=0; i<e; i++) {
+            System.out.println("For Edge "+(i+1));
+            System.out.print("Enter the source and destination: ");
+            int s = sc.nextInt();
+            int d = sc.nextInt();
+            g.addEdge(s,d);
         }
-        System.out.println("The adjacency matrix is: ");
-        g.display();
+        System.out.print("Enter the source for Traversal: ");
+        int s = sc.nextInt();
+        g.BFS(s);
+        sc.close();
     }
 }
--- Exp8/graphds/LLGraph.java
+++ Exp8/graphds/LLGraph.java
@@ -1,4 +1,52 @@
 package graphds;
+import java.util.*;
 public class LLGraph {
-    
+    private LinkedList<Integer> adj[];
+    public LLGraph(int v) {
+        adj = new LinkedList[v];
+        for(int i=0; i<v; i++)
+            adj[i] = new LinkedList<Integer>();
+    }
+    public void addEdge(int s, int d) {
+        adj[s].add(d);
+        adj[d].add(s);
+    }
+    public void BFS(int s) {
+        boolean visited[] = new boolean[adj.length];
+        int parent[] = new int[adj.length];
+        Queue<Integer> q = new LinkedList<Integer>();
+        q.add(s);
+        visited[s] = true;
+        parent[s] = -1;
+        while(!q.isEmpty()) {
+            int v = q.poll();
+            System.out.print(v+" ");
+            for(int i: adj[v]) {
+                if(!visited[i]) {
+                    q.add(i);
+                    parent[i] = v;
+                    visited[i] = true;
+                }
+            }
+        }
+    }
+    public void DFS(int s) {
+        boolean visited[] = new boolean[adj.length];
+        int parent[] = new int[adj.length];
+        Stack<Integer> st = new Stack<Integer>();
+        st.push(s);
+        visited[s] = true;
+        parent[s] = -1;
+        while(!st.isEmpty()) {
+            int v = st.pop();
+            System.out.print(v+" ");
+            for(int i: adj[v]) {
+                if(!visited[i]) {
+                    st.push(i);
+                    parent[i] = v;
+                    visited[i] = true;
+                }
+            }
+        }
+    }
 }
