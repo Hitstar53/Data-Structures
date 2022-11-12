391d3d603a1f351d5afe7a3fbd7113298e2caed3
Hatim Sawai <hatimsawai76@gmail.com>
2022-11-12 12:19:00 +0530

    graph traversal final
    Signed-off-by: Hatim Sawai <hatimsawai76@gmail.com>

--- Exp8/GraphCheck.java
+++ Exp8/GraphCheck.java
@@ -15,9 +15,31 @@ public class GraphCheck {
             int d = sc.nextInt();
             g.addEdge(s,d);
         }
-        System.out.print("Enter the source for Traversal: ");
-        int s = sc.nextInt();
-        g.BFS(s);
+        System.out.println("The Graph is:");
+        g.printGraph();
+        while(true) {
+            System.out.print("Enter the source for Traversal: ");
+            int s = sc.nextInt();
+            System.out.println("Choose the type of Traversal:\n1. BFS\n2. DFS");
+            int ch = sc.nextInt();
+            switch(ch) {
+                case 1:
+                    System.out.println("BFS Traversal:");
+                    g.BFS(s);
+                    break;
+                case 2:
+                    System.out.println("DFS Traversal:");
+                    g.DFS(s);
+                    break;
+                default:
+                    System.out.println("Invalid Choice!");
+            }
+            System.out.print("Do you want to continue? (0=no/1=yes): ");
+            int c = sc.nextInt();
+            if(c==0) {
+                break;
+            }
+        }
         sc.close();
     }
 }
--- Exp8/graphds/LLGraph.java
+++ Exp8/graphds/LLGraph.java
@@ -1,52 +1,86 @@
 package graphds;
 import java.util.*;
 public class LLGraph {
-    private LinkedList<Integer> adj[];
+    private ArrayList<ArrayList<Integer>> adj;
     public LLGraph(int v) {
-        adj = new LinkedList[v];
-        for(int i=0; i<v; i++)
-            adj[i] = new LinkedList<Integer>();
+        adj = new ArrayList<ArrayList<Integer>>(v);
+        for(int i=0; i<v; i++) {
+            adj.add(new ArrayList<Integer>());
+        }
     }
     public void addEdge(int s, int d) {
-        adj[s].add(d);
-        adj[d].add(s);
+        adj.get(s).add(d);
+        adj.get(d).add(s);
+    }
+    public void printGraph() {
+        for(int i=0; i<adj.size(); i++) {
+            System.out.print(i+" -> ");
+            for(int j=0; j<adj.get(i).size(); j++) {
+                System.out.print(adj.get(i).get(j)+" ");
+            }
+            System.out.println();
+        }
     }
     public void BFS(int s) {
-        boolean visited[] = new boolean[adj.length];
-        int parent[] = new int[adj.length];
+        boolean[] visited = new boolean[adj.size()];
+        int[] parent = new int[adj.size()];
+        int[] level = new int[adj.size()];
         Queue<Integer> q = new LinkedList<Integer>();
         q.add(s);
-        visited[s] = true;
         parent[s] = -1;
+        visited[s] = true;
+        level[s] = 0;
         while(!q.isEmpty()) {
-            int v = q.poll();
-            System.out.print(v+" ");
-            for(int i: adj[v]) {
-                if(!visited[i]) {
-                    q.add(i);
-                    parent[i] = v;
-                    visited[i] = true;
+            int v = q.remove();
+            System.out.println(v+"(level "+level[v]+")");
+            for(int i=0;i<adj.get(v).size();i++) {
+                int w = adj.get(v).get(i);
+                if(!visited[w]) {
+                    level[w] = level[v]+1;
+                    q.add(w);
+                    visited[w] = true;
+                    parent[w] = v;
                 }
             }
         }
     }
     public void DFS(int s) {
-        boolean visited[] = new boolean[adj.length];
-        int parent[] = new int[adj.length];
+        boolean[] visited = new boolean[adj.size()];
+        int[] parent = new int[adj.size()];
         Stack<Integer> st = new Stack<Integer>();
+        int dfs[] = new int[adj.size()];
+        int[] start = new int[adj.size()];
+        int[] end = new int[adj.size()];
+        int time = 1,j=0;
         st.push(s);
+        start[s] = time;
         visited[s] = true;
         parent[s] = -1;
+        dfs[j++] = s;
         while(!st.isEmpty()) {
-            int v = st.pop();
-            System.out.print(v+" ");
-            for(int i: adj[v]) {
-                if(!visited[i]) {
-                    st.push(i);
-                    parent[i] = v;
-                    visited[i] = true;
+            int v = st.peek();
+            boolean flag = false;
+            for(int i=0;i<adj.get(v).size();i++) {
+                int w = adj.get(v).get(i);
+                if(!visited[w]) {
+                    st.push(w);
+                    start[w] = ++time;
+                    visited[w] = true;
+                    parent[w] = v;
+                    flag = true;
+                    dfs[j] = w;
+                    j++;
+                    break;
                 }
             }
+            if(!flag) {
+                end[v] = ++time;
+                st.pop();
+            }
+        }
+        System.out.println("DFS Traversal:");
+        for(int i=0;i<j;i++) {
+            System.out.println(dfs[i]+" (start: "+start[dfs[i]]+", end: "+end[dfs[i]]+")");
         }
     }
 }
